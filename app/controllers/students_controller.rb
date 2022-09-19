02e1911e9c900dfde8eb72e29c580940c4b02ae1
Quade Houston <quadehouston@gmail.com>
2022-09-19 19:12:02 -0400

    commit tree 241f8bf25974bcc19370e866c47fa1cbe3fe032c

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,16 @@
 class StudentsController < ApplicationController
 
   def index
-    students = Student.all
+    if params[:name]
+      students = Student.select{|student| student.first_name == params[:name] || student.last_name == params[:name]}
+    else
+      students = Student.all
+   end
     render json: students
+  end
+
+  def show
+    render json: student_id = Student.find_by(id: params[:id])
   end
 
 end
